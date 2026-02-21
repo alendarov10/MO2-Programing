649c2cfddeec5a535401af98c9d40dbfef61acf4
Ibrahim Alendar <alendarov_10@abv.bg>
2026-02-21 14:43:41 +0200

    New Project

---
+++ M02-Programing/S02-LIsts/P02-ListInputOutputLine/Program.cs
@@ -0,0 +1,12 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        List<int> nums = Console.ReadLine()!.Split(" ").Select(int.Parse).ToList();
+
+        for (int i = 0; i < nums.Count; i++)
+        {
+            Console.WriteLine($"nums[{i}] = {nums[i]}");
+        }
+    }
+}

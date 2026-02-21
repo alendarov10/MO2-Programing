4d2a1d9e239e11f98e90c172fdeb5b15822d82d1
Ibrahim Alendar <alendarov_10@abv.bg>
2026-02-21 16:18:05 +0200

    Exercises - string processing

---
+++ M02-Programing/S03-Strings/P03-CharMethods/Program.cs
@@ -0,0 +1,10 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine(char.IsDigit('9'));
+        Console.WriteLine(char.IsLetter('j'));
+        Console.WriteLine(char.IsLower('A'));
+        Console.WriteLine(char.IsWhiteSpace('\t'));
+    }
+}
